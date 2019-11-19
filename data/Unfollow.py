94ccf8ed04a4cbc257b6d923a2f314afef44b9d4
DulLah <52864765+dz-id@users.noreply.github.com>
2019-11-19 21:44:19 +0700

    Add files via upload

---
+++ data/Unfollow.py
@@ -0,0 +1,54 @@
+#!usr/bin/python3.7
+#Author: DulLah ©2019
+#contact: fb.me/dulahz
+#github: github.com/dz-id
+
+try:
+	import sys, requests, json
+	from multiprocessing.pool import ThreadPool
+	
+	if sys.platform in ['linux','linux2']:
+		W = '\033[0m'
+		G = '\033[1;92m'
+		R = '\033[1;91m'
+	else:
+		W = ''
+		G = ''
+		R = ''
+	
+	HD = {'User-Agent' : open('UserAgent/ua.txt').read()}
+	token = open('log/token').read()
+	id = []
+except Exception as E:
+	print(W + '[' + R + '!' + W + ']' + R + str(E))
+try:
+	print(W + '\n[' + G + '*' + W + '] Get user id... ')
+	r = requests.get('https://graph.facebook.com/me/subscribedto?limit=5000&access_token=' + token,headers = HD)
+	a = json.loads(r.text)
+	
+	print()
+	for i in a['data']:
+		id.append(i['id'])
+		b = requests.post('https://graph.facebook.com/' + str(i['id']) + '/subscribers?method=delete&access_token=' + token,headers = HD)
+		try:
+			error = b.json()['error']['message']
+			print(W + '[' + R + '×' + W + '] ' + G + str(i['name']) + W + ' - failed')
+		except TypeError:
+			print(W + '[' + G + '-' + W + '] ' + G + str(i['name']) + W + ' - unfollow')
+	if len(id) == 0:
+		print(W + '[' + R + '!' + W + '] ' + R + 'no followers found!')
+	else:
+		print(W + '\n[' + G + '•' + W + '] done!')
+
+except KeyError:
+	print(W + '\n[' + R + '!' + W + '] ' + R + 'error!')
+	print(W + '[' + R + '!' + W + '] ' + R + 'stopped!')
+	sys.exit()
+except requests.exceptions.ConnectionError:
+	print(W + '\n[' + R + '!' + W + '] ' + R + 'connections error!')
+	print(W + '[' + R + '!' + W + '] ' + R + 'stopped!')
+	sys.exit()
+except KeyboardInterrupt:
+	print(W + '\n[' + R + '!' + W + '] ' + R + 'Key interrupt!')
+	print(W + '[' + R + '!' + W + '] ' + R + 'stopped!')
+	sys.exit()
